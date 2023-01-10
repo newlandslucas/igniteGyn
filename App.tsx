d586e0c4768f7d3270a56e8f5afa63569de051f5
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2023-01-10 10:49:21 -0300

    feature: external fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold})
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{flex: 1,alignItems: 'center', justifyContent: 'center'}}>
+      {fontsLoaded ? <Text style={{fontFamily: 'Roboto_700Bold'}}>Hello World</Text> : <View />}
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
