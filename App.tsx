bf0daa7c326ee356e3367673dfbd4b69f8ba9b5e
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2023-01-12 16:37:52 -0300

    feature: Signin screen finished

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider} from 'native-base';
 import { Text, View } from 'react-native';
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
+import { Loading } from './src/components/Loading';
+import { THEME } from './src/theme';
+import { Signin } from './src/screens/Signin';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold})
 
   return (
-    <View style={{flex: 1,alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024'}}>
-      {fontsLoaded ? <Text style={{fontFamily: 'Roboto_700Bold'}}>Hello World</Text> : <View />}
+    <NativeBaseProvider theme={THEME}>
+      {fontsLoaded ? <Signin /> : <Loading />}
       <StatusBar style="light" translucent backgroundColor='transparent'/>
-    </View>
+    </NativeBaseProvider>
   );
 }
